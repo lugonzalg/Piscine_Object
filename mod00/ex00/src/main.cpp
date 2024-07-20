0798366c746ef4e01110bf664861bc7d06397c6d
lugarte <lugarte>
2024-07-20 14:41:24 +0000

    change: simple main to test

--- mod00/ex00/src/main.cpp
+++ mod00/ex00/src/main.cpp
@@ -6,20 +6,12 @@
 int main()
 {
 	Account accountA = Account();
-	accountA.id = 0;
-	accountA.value = 100;
 
 	Account accountB = Account();
-	accountB.id = 1;
-	accountB.value = 100;
 
 	Bank bank = Bank();
-	bank.liquidity = 999;
-	bank.clientAccounts.push_back(&accountA);
-	bank.clientAccounts.push_back(&accountB);
-
-	bank.liquidity -= 200;
-	accountA.value += 400;
+	bank.add_client(&accountA);
+	bank.add_client(&accountB);
 
 	std::cout << "Account : " << std::endl;
 	std::cout << accountA << std::endl;
