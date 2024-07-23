47fcd6ceab2b8fdb674a27f72d561266042b722f
lugonzal <lugarte>
2024-07-23 22:10:11 +0000

    change: new attribute name

--- mod01/ex00/inc/Worker.hpp
+++ mod01/ex00/inc/Worker.hpp
@@ -1,24 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+
 #include "Position.hpp"
 #include "Statistic.hpp"
 #include "Tool.hpp"
 
+class Workshop;
+
 class Worker {
 
     private:
+        const std::string _name;
         Position _coordonnee;
         Statistic _stat;
         Tool* _tool;
 
     public:
 
-        Worker(int x, int y, int z, int exp, int level);
+        Worker(const std::string name, int x, int y, int z, int exp, int level);
         ~Worker();
 
         const Position& get_position() const;
         const Statistic& get_stat() const;
+        const std::string& get_name() const;
+
         bool equip_tool(Tool *tool);
         void unequip_tool();
 
--- mod01/ex00/src/Worker.cpp
+++ mod01/ex00/src/Worker.cpp
@@ -1,13 +1,13 @@
 #include "Worker.hpp"
 
-Worker::Worker(int x, int y, int z, int exp, int level) : 
-    _coordonnee(x, y, z), _stat(exp, level), _tool(NULL) {
+Worker::Worker(const std::string name, int x, int y, int z, int exp, int level) : 
+    _name(name), _coordonnee(x, y, z), _stat(exp, level), _tool(NULL) {
 
-    std::cout << "Worker constructor called\n";
+    std::cout << "[WORKER] - constructor called\n";
     std::cout << *this;
 }
 Worker::~Worker() {
-    std::cout << "Worker destructor called\n";
+    std::cout << "[WORKER] - destructor called\n";
 }
 
 const Position& Worker::get_position() const {
@@ -15,6 +15,10 @@ const Position& Worker::get_position() const {
 }
 const Statistic& Worker::get_stat() const {
     return this->_stat;
+}
+
+const std::string& Worker::get_name() const {
+    return this->_name;
 }
 
 bool Worker::equip_tool(Tool *tool) {
@@ -34,8 +38,9 @@ void Worker::unequip_tool() {
 }
 
 std::ostream& operator << (std::ostream& os, const Worker& worker) {
-    os << "Worker info: "
-        << "\ncoordonne: \n" << worker.get_position()
-        << "\nstat: \n" << worker.get_stat() << std::endl;
+    os << "[WORKER]:"
+        << "\n\tname: " << worker.get_name()
+        << "\n\t" << worker.get_position()
+        << "\n\t" << worker.get_stat() << std::endl;
     return os;
 }
