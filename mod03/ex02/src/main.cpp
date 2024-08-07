2c9426552631fc192df422428658385b22206099
lugonzal <lugarte>
2024-08-07 19:54:36 +0000

    change: new Triangle and Circle us

--- mod03/ex02/src/main.cpp
+++ mod03/ex02/src/main.cpp
@@ -1,4 +1,6 @@
 #include "rectangle.hpp"
+#include "triangle.hpp"
+#include "circle.hpp"
 
 float get_area(const Shape& shape) {
     return shape.area();
@@ -10,8 +12,16 @@ float get_perimeter(const Shape& shape) {
 
 int main() {
     Rectangle rectangle(1.0f, 2.0f);
+    Triangle triangle(4.0f, 4.0f, 5.0f);
+    Circle circle(1.0f);
 
     std::cout << "Area: " << get_area(rectangle) << std::endl;
     std::cout << "Perimeter: " << get_perimeter(rectangle) << std::endl;
+
+    std::cout << "Area: " << get_area(triangle) << std::endl;
+    std::cout << "Perimeter: " << get_perimeter(triangle) << std::endl;
+
+    std::cout << "Area: " << get_area(circle) << std::endl;
+    std::cout << "Perimeter: " << get_perimeter(circle) << std::endl;
     return 0;
 }
